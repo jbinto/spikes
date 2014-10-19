5ba086968b97a32571cfccd8e4e4269d257c5cd0
Jesse Buchanan <hello@jessebuchanan.ca>
2014-10-18 22:37:42 -0400

    yo angular

---
+++ 02-angular-google-maps/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name 02AngularGoogleMapsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the 02AngularGoogleMapsApp
+ */
+angular.module('02AngularGoogleMapsApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ 02-angular-google-maps/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name 02AngularGoogleMapsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the 02AngularGoogleMapsApp
+ */
+angular.module('02AngularGoogleMapsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
